2af9171be785a916be184ce2ad0531ce27e07b36
nils.lueem <nils.lueem@inpeek.ch>
2025-06-09 22:39:54 +0200

    add: header

--- app/src/app/shared/layout/header.ts
+++ app/src/app/shared/layout/header.ts
@@ -1,18 +1,15 @@
-import { CdkMenuModule } from '@angular/cdk/menu';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [CdkMenuModule],
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
 })
 export class HeaderComponent {
-  protected title = 'wiewarm';
+  protected title = 'wiewarm.ch';
   protected darkMode = false;
 
-  toggleDarkMode() {
+  public toggleDarkMode() {
     this.darkMode = !this.darkMode;
     document.body.classList.toggle('dark-mode', this.darkMode);
   }
