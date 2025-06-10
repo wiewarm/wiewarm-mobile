2407ca5a287fe6901f9c6171946ef0329611509e
nils.lueem <nils.lueem@inpeek.ch>
2025-06-10 13:22:03 +0200

    use basic cdk table

--- app/src/app/features/list/list.ts
+++ app/src/app/features/list/list.ts
@@ -1,29 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, resource, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CdkMenuModule } from '@angular/cdk/menu';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkTableModule } from '@angular/cdk/table';
 import { BadItem } from 'src/app/shared/interfaces/bad-item.interface';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.html',
-  imports: [CommonModule, FormsModule, CdkMenuModule, ScrollingModule],
+  imports: [CommonModule, FormsModule, ScrollingModule, CdkTableModule],
   styleUrl: './list.scss',
 })
 export class ListComponent {
   private readonly BAD_ITEM_URL = '/api/v1/temperature/all_current.json/0';
 
   searchInput = signal('');
-  sortType = true;
-
-  toggleSortDirection() {
-    this.sortType = !this.sortType;
-  }
-
-  sortDirLabel() {
-    return this.sortType ? 'aufsteigend' : 'absteigend';
-  }
+  readonly displayedColumns = ['bad', 'ort'];
 
   itemsResource = resource<BadItem[], unknown>({
     loader: ({ abortSignal }) =>
@@ -37,20 +29,10 @@ export class ListComponent {
     const term = this.searchInput().toLowerCase();
     const items = this.itemsResource.value() ?? [];
 
-    const filtered = items.filter((item: any) => {
+    return items.filter((item: any) => {
       const hay =
         `${item.bad} ${item.becken} ${item.ort} ${item.plz}`.toLowerCase();
       return !term || hay.includes(term);
     });
-
-    // Sortieren nach Datum
-    const sorted = [...filtered].sort((a, b) => {
-      const ta = new Date(a.date).getTime();
-      const tb = new Date(b.date).getTime();
-      const cmp = ta - tb;
-      return this.sortType ? cmp : -cmp;
-    });
-
-    return sorted;
   });
 }
