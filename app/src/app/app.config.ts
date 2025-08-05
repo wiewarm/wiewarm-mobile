8c3cbe38ecff6da93c32e802f224e2b5143845f2
nils.lueem <nils.lueem@inpeek.ch>
2025-08-05 13:15:50 +0200

    test(animations): simple animations

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
@@ -14,5 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimations(),
   ],
 };
--- app/src/app/features/bad-overview/bad-item/bad-item.ts
+++ app/src/app/features/bad-overview/bad-item/bad-item.ts
@@ -6,12 +6,40 @@ import { BadItem } from 'src/app/shared/services/interfaces/bad-item.interface';
 import { FavoriteService } from 'src/app/shared/services/favorite.service';
 import { isOlderThanOneMonth } from 'src/app/shared/util/date.util';
 import { temperatureClass } from 'src/app/shared/util/temperature.util';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+} from 'node_modules/@angular/animations';
 
 @Component({
   selector: 'app-bad-item',
   templateUrl: './bad-item.html',
   styleUrl: './bad-item.scss',
   imports: [CommonModule, RouterModule, CdkAccordionModule],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate(
+          '200ms ease-in-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ]),
+    ]),
+    trigger('expandCollapse', [
+      transition(':enter', [
+        style({ height: 0, opacity: 0, overflow: 'hidden' }),
+        animate('200ms ease-in-out', style({ height: '*', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ overflow: 'hidden' }),
+        animate('200ms ease-in-out', style({ height: 0, opacity: 0 })),
+      ]),
+    ]),
+  ],
+  host: { '[@fadeIn]': '' },
 })
 export class BadItemComponent {
   isOlderThanOneMonth = isOlderThanOneMonth;
