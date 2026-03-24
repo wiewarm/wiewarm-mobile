8424ee42402275412cabe78b6fe2c5a021721e4d
nils.lueem <nils.lueem@inpeek.ch>
2026-03-24 14:07:23 +0100

    feat(detail): improve image toggle and responsive detail layout

--- app/src/app/features/bad-detail/img-item/img-item.ts
+++ app/src/app/features/bad-detail/img-item/img-item.ts
@@ -1,23 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import type { OwlOptions } from 'ngx-owl-carousel-o';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { environment } from '../../../../environments/environment';
+import { IconComponent } from '../../../shared/layout/icon/icon';
 import type { BadImage } from '../../../shared/services/interfaces/bad-image.interface';
 import type { BadDetail } from 'src/app/shared/services/interfaces/bad-detail.interface';
 
 @Component({
   selector: 'app-img-item',
-  imports: [CarouselModule],
+  imports: [CarouselModule, IconComponent],
   templateUrl: './img-item.html',
   styleUrl: './img-item.scss',
 })
 export class ImgItemComponent {
   readonly detail = input.required<BadDetail>();
+  protected readonly isOpen = signal(false);
 
   readonly carouselOptions: OwlOptions = {
     loop: true,
-    dots: false,
-    nav: false,
     items: 1,
     responsive: {
       768: {
@@ -25,6 +25,8 @@ export class ImgItemComponent {
       },
     },
   };
+
+  readonly toggle = () => this.isOpen.update((open) => !open);
 
   imageSrc(image: BadImage): string {
     const base = environment.imageBase.replace(/\/?$/, '/');
